98afd9b5b94724a332b442323b7c5fcbd10ed721
Carlos Tello <carlostello@Carloss-MacBook-Pro.local>
2023-12-01 02:04:00 +0800

    add Documentation and improve mandatory fields

--- src/products/controllers/products.controller.ts
+++ src/products/controllers/products.controller.ts
@@ -2,12 +2,28 @@ import { Controller, Get, Param, Query } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { Product } from '../entities/product.entity';
 import { ProductsService } from '../services/products.service';
+import { ApiQuery, ApiTags } from '@nestjs/swagger';
 
 @Controller('products')
+@ApiTags('Products')
+
 export class ProductsController {
   constructor(private readonly productService: ProductsService) {}
 
+  /**
+   * Handles HTTP GET requests to the base route /products.
+   * Supports optional query parameters such as limit, skip, filter, and select.
+   * @param limit - Optional parameter for limiting the number of results.
+   * @param skip - Optional parameter for skipping a certain number of results.
+   * @param filter - Optional parameter for filtering results based on a criteria.
+   * @param select - Optional parameter for selecting specific fields in the results.
+   * @returns Observable<Product[]> - An observable emitting an array of products.
+   */
   @Get()
+  @ApiQuery({ name: 'limit', required: false})
+  @ApiQuery({name: 'skip', required: false})
+  @ApiQuery({name: 'filter', required: false})
+  @ApiQuery({name: 'select', required: false})
   getProducts(
     @Query('limit') limit?: number,
     @Query('skip') skip?: number,
@@ -17,6 +33,11 @@ export class ProductsController {
     return this.productService.getProducts(limit, skip, filter, select);
   }
 
+  /**
+   * Handles HTTP GET requests to a specific route /products/:id.
+   * @param id - The product ID obtained from the route parameter.
+   * @returns Observable<Product> - An observable emitting a single product.
+   */
   @Get(':id')
   getProductById(
     @Param('id') id: number
