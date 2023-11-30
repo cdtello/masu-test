c56479204daf8fa700975da727ce3e970ee68866
Carlos Tello <carlostello@Carloss-MacBook-Pro.local>
2023-12-01 03:33:17 +0800

    test deploy 2

--- src/products/services/products.service.ts
+++ src/products/services/products.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { Observable, from } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
-import { Product } from '../entities/product.entity'; // Asegúrate de importar correctamente tu modelo de producto
+import { Product } from '../entities/product.entity'; 
+import axios, { AxiosResponse } from 'axios';
 
 @Injectable()
 export class ProductsService {
@@ -14,21 +15,24 @@ export class ProductsService {
    * @param select - Optional criteria for selecting specific fields in the results.
    * @returns Observable<Product[]> - An observable emitting an array of products.
    */
-  getProducts(limit = 10, skip = 0, filter = null, select = null): Observable<Product[]> {
-    const urlSelect = select === null 
-      ? '' 
-      : `&select=${select}`;
-    const urlFilter = filter === null 
-      ? '?' 
-      : `/search?q=${filter}&`;
-      
+  getProducts(
+    limit = 10,
+    skip = 0,
+    filter = null,
+    select = null
+  ): Observable<Product[]> {
+    const urlSelect = select === null ? '' : `&select=${select}`;
+    const urlFilter = filter === null ? '?' : `/search?q=${filter}&`;
+
     const url = `https://dummyjson.com/products${urlFilter}limit=${limit}&skip=${skip}${urlSelect}`;
-    return from(fetch(url)).pipe(
-      switchMap(async (response) => {
-        const data = await response.json();
-        const {total, skip, limit, products} = data;
-        return products.map((productData: any) => productData as Product);
-      }),
+
+    return from(
+      axios.get<{ products: Product[] }>(url)
+    ).pipe(
+      switchMap((response: AxiosResponse<{ products: Product[] }>) => {
+        const { products } = response.data;
+        return from([products]); // Wrap the array in another array to match Observable<Product[]>
+      })
     );
   }
 
@@ -39,11 +43,13 @@ export class ProductsService {
    */
   getProductById(id): Observable<Product> {
     const url = `https://dummyjson.com/products/${id}`;
-    return from(fetch(url)).pipe(
-      switchMap(async (response) => {
-        const data = await response.json();
-        return data as Product;
-      }),
+
+    return from(
+      axios.get<Product>(url)
+    ).pipe(
+      switchMap((response: AxiosResponse<Product>) => {
+        return from([response.data]); // Wrap the data in an array to match Observable<Product> type
+      })
     );
   }
 }
